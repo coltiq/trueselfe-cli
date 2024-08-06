3691e5a9e4736beffac226c7e4dca55e640661f5
coltiq <dev@coltiq.com>
2024-08-06 10:41:13 -0500

    Feat: Ensure init is run first

--- cmd/init.go
+++ cmd/init.go
@@ -4,6 +4,9 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/coltiq/trueselfe-cli/data"
 	"github.com/spf13/cobra"
 )
@@ -14,7 +17,18 @@ var initCmd = &cobra.Command{
 	Long:  `Initialize a new True Selfe database with tables: pillars, goals, completions.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		data.CreatePillarTable()
+		data.CreateStepsTable()
+		createInitFile()
 	},
+}
+
+func createInitFile() {
+	file, err := os.Create(".initialized")
+	if err != nil {
+		fmt.Println("Error creating initialization file:", err)
+		return
+	}
+	defer file.Close()
 }
 
 func init() {
