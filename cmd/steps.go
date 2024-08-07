5ab0c42bef921431ec4ef0863dc6f64b0147a84b
Coltiq <dev@coltiq.com>
2024-08-07 08:39:47 -0500

    Feat: Add Step

--- cmd/steps.go
+++ cmd/steps.go
@@ -5,9 +5,14 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 
+	"github.com/coltiq/trueselfe-cli/data"
 	"github.com/spf13/cobra"
 )
+
+var addParams []string
 
 var stepsCmd = &cobra.Command{
 	Use:   "steps",
@@ -15,10 +20,24 @@ var stepsCmd = &cobra.Command{
 	Long: `This allows you to display an overview of all your pillars along with the steps you created
 to improve these pillars of your life.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("steps called")
+		if len(addParams) == 4 {
+			pillarID, err := strconv.Atoi(addParams[0])
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+			name := addParams[1]
+			description := addParams[2]
+			frequency := addParams[3]
+			data.AddStep(pillarID, name, description, frequency)
+		} else if len(addParams) > 0 {
+			log.Println("Error: -a flag requires exactly 4 parameters: pillarID, name, description, frequency")
+		} else {
+			fmt.Println("Steps Called")
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(stepsCmd)
+	stepsCmd.Flags().StringSliceVarP(&addParams, "add", "a", nil, "Add a new step with parameters: pillarID, name, description, frequency")
 }
--- data/stepsDB.go
+++ data/stepsDB.go
@@ -7,7 +7,7 @@ func CreateStepsTable() {
 		"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		"pillar_id" INTEGER NOT NULL,
 		"name" TEXT NOT NULL,
-		"description" TEXT,
+		"description" TEXT NOT NULL,
 		"created" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 		"frequency" TEXT NOT NULL,
 		FOREIGN KEY (pillar_id) REFERENCES pillars(id) ON DELETE CASCADE,
@@ -21,4 +21,20 @@ func CreateStepsTable() {
 
 	statement.Exec()
 	log.Println("Steps Table Created")
+}
+
+func AddStep(pillarID int, name, description, frequency string) {
+	insertStepSQL := `INSERT INTO steps (pillar_id, name, description, frequency) VALUES (?, ?, ?, ?)`
+	statement, err := db.Prepare(insertStepSQL)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer statement.Close()
+
+	_, err = statement.Exec(pillarID, name, description, frequency)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	log.Println("Step Added")
 }
